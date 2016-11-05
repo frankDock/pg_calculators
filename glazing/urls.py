9bc70cd0625ba7632fc3cad648adc1fef907e4e2
alon <alon@schematech.co.za>
2016-11-05 18:39:50 +0200

    new projec form

--- glazing/urls.py
+++ glazing/urls.py
@@ -6,6 +6,6 @@ app_name = 'glazing'
 urlpatterns = [
     url(r'^$', views.glazing_project_list, name='glazing_project_list'),
     #url(r'^glazing/(?P<pk>\d+)/$', views.glazing_project_detail, name='glazing_project_detail'),
-    #url(r'^glazing/new/$', views.glazing_project_new, name='glazing_project_new'),
+    url(r'^glazing/new/$', views.glazing_project_new, name='glazing_project_new'),
 ]
 
